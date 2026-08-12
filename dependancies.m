3fc3444c439135e3ac28123dfd9b70817d021fbd
Sven Leach <info@svenno.de>
2026-08-12 22:48:02 +0200

    Linux compatibile dependancies

--- dependancies.m
+++ dependancies.m
@@ -2,29 +2,29 @@
 % External dependancies ---------------------------------------------------
 % *************************************************************************
 % GEDAI
-addpath(genpath('..\GEDAI-master')) % https://github.com/SvennoNito/dusk2dawn
+addpath(genpath(['..' filesep 'GEDAI-master'])) % https://github.com/SvennoNito/dusk2dawn
 
 % BIDS
-addpath('..\bids-matlab') % https://github.com/bids-standard/bids-matlab
+addpath(['..' filesep 'bids-matlab']) % https://github.com/bids-standard/bids-matlab
 
 % Sophias preprocessing packages
 % addpath('..\chART')
-addpath('..\eeg-oscillations') % https://github.com/SvennoNito/eeg-oscillations
+addpath(['..' filesep 'eeg-oscillations']) % https://github.com/SvennoNito/eeg-oscillations
 % -> oscip.fit_fooof
 
 % EEGLAB
-eeglabpath = '..\EEGLAB'; % https://sccn.ucsd.edu/eeglab/download.php
+eeglabpath = ['..' filesep 'eeglab']; % https://sccn.ucsd.edu/eeglab/download.php
 addpath(eeglabpath) 
 
+% EEGLAB extensions
+addpath(['..' filesep 'cleanline'])
+addpath(['..' filesep 'zapline-plus'])
+addpath(['..' filesep 'bva-io'])
+
 % Brainstorm
-addpath('..\brainstorm3')
+addpath(['..' filesep 'brainstorm3'])
 
-% EEGLAB requires extensions for importing BrainVision files.
-% Run `eeglab`, then install needed plugins via: File -> Manage EEGLAB extensions
-% Extensions: 
-% -> EEG-BIDS 
-% -> bva-io1.73
-
+% EEGLAB startup
 if exist('pop_loadset', 'file') ~= 2
     % Checks if EEGLAB has previously been called in this session. If not,
     % call EEGLAB and close the pop-up window.
